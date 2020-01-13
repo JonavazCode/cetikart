145a1e128ee6d1b37d7516e088a08d2962e66c99
OgeidSR <39096636+OgeidSR@users.noreply.github.com>
2020-01-13 14:17:24 -0600

    Avance de historial

--- Assets/Historial.cs
+++ Assets/Historial.cs
@@ -8,6 +8,7 @@ public class Historial : MonoBehaviour
     public int numero;
     public Text historial;
     private Checkpoint_Meta ganador;
+    public int selec_profesor;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,10 +30,19 @@ public class Historial : MonoBehaviour
         {
             Debug.Log(jugador);
             numero++;
-            //jugador.Replace("_car", " ");
-            jugador.Replace("a","z");
-            historial.text += "\n" + numero + "° Lugar: "+ jugador;
-            //jugador.Replace("ulyses_carE", "Profesor Ulyses");
+          
+            historial.text += jugador.Contains("molina_car") ? "\n" + numero + "° Lugar: " + "Profesor Molina" : "";
+            historial.text += jugador.Contains("ulyses_car") ? "\n" + numero + "° Lugar: " + "Profesor Ulyses" : "";
+            historial.text += jugador.Contains("nino_car") ? "\n" + numero + "° Lugar: " + "Profesor Niño" : "";
+            historial.text += jugador.Contains("areli_car") ? "\n" + numero + "° Lugar: " + "Profesora Areli" : "";
+            historial.text += jugador.Contains("sergio_car") ? "\n" + numero + "° Lugar: " + "Profesor Sergio" : "";
+            historial.text += jugador.Contains("coco_car") ? "\n" + numero + "° Lugar: " + "Profesor Ismael" : "";
+            historial.text += jugador.Contains("gussa_car") ? "\n" + numero + "° Lugar: " + "Profesora Susana" : "";
+            historial.text += jugador.Contains("agentek_car") ? "\n" + numero + "° Lugar: " + "Profesor Luis Rene" : "";
+
+
+            //historial.text += "\n" + numero + "° Lugar: "+ jugador.Replace("_car","");
+
         }
 
 
---
+++ Assets/icono_Profesores.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class icono_Profesores : MonoBehaviour
+{
+    public Historial profesor;
+    public Sprite profesor_ulyses = Resources.Load<Sprite>("Profesor_Ulyses_Boton");
+    public Sprite profesor_rene = Resources.Load<Sprite>("Profesor_Ruben_Boton");
+    public Sprite profesor_areli = Resources.Load<Sprite>("Profesor_Areli_Boton");
+    public Sprite profesor_ismael = Resources.Load<Sprite>("Profesor_Coco_Boton");
+    public Sprite profesor_sergio = Resources.Load<Sprite>("Profesor_Sergio_Boton");
+    public Sprite profesor_nino = Resources.Load<Sprite>("Profesor_Niño_Boton");
+    public Sprite profesor_molina = Resources.Load<Sprite>("Profesor_Molina_Boton");
+    public Sprite profesor_susana = Resources.Load<Sprite>("Profesor_Susana_Boton");
+    // Start is called before the first frame update
+    void Start()
+    {
+        profesor = FindObjectOfType<Historial>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (profesor.selec_profesor == 0)
+        {
+            this.gameObject.GetComponent<Image>().sprite = profesor_ulyses;
+        }
+        if (profesor.selec_profesor == 1)
+        {
+            this.gameObject.GetComponent<Image>().sprite = profesor_rene;
+        }
+        if (profesor.selec_profesor == 2)
+        {
+            this.gameObject.GetComponent<Image>().sprite = profesor_areli;
+        }
+        if (profesor.selec_profesor == 3)
+        {
+            this.gameObject.GetComponent<Image>().sprite = profesor_ismael;
+        }
+        if (profesor.selec_profesor == 4)
+        {
+            this.gameObject.GetComponent<Image>().sprite = profesor_sergio;
+        }
+        if (profesor.selec_profesor == 5)
+        {
+            this.gameObject.GetComponent<Image>().sprite = profesor_nino;
+        }
+        if (profesor.selec_profesor == 6)
+        {
+            this.gameObject.GetComponent<Image>().sprite = profesor_molina;
+        }
+        if (profesor.selec_profesor == 7)
+        {
+            this.gameObject.GetComponent<Image>().sprite = profesor_susana;
+        }
+
+    }
+}
