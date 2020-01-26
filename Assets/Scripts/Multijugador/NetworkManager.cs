99bf679a47b20ada6298ad1879db10d5b06bfc19
Zokler <36612168+Zokler@users.noreply.github.com>
2020-01-26 00:57:24 -0600

    Ya crea la sala con nombre y password!

--- Assets/Scripts/Multijugador/NetworkManager.cs
+++ Assets/Scripts/Multijugador/NetworkManager.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
-using System;
 using Photon.Realtime;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    [Header("Exception Management")]
+    [Header("Error's text")]
     public Text GameNameText;
+    public Text RoomNameText;
 
     [Header("Loggin UI")]
     public GameObject LoginUIPanel;
@@ -28,6 +28,9 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     [Header("Create Room Panel")]
     public GameObject CreateRoomUIPanel;
+    public InputField RoomNameInputField;
+    public InputField MaxPlayersInputField;
+    public InputField PasswordInputField;
 
     [Header("Inside Room Panel")]
     public GameObject InsideRoomUIPanel;
@@ -77,7 +80,39 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         ActivatePanel(GameOptionsUIPanel.name);  
     }
 
+    public void OnCreateRoomButtonClicked()
+    {
+        if (string.IsNullOrEmpty(RoomNameInputField.text))
+        {
+            RoomNameText.text = "Rellenar Campos!";
+        }
+        else
+        {
+            string roomName = RoomNameInputField.text;
+            int maxPlayers = int.Parse(MaxPlayersInputField.text);
+            if (maxPlayers > 8)
+                maxPlayers = 8;
+            string pass = PasswordInputField.text;
+            if (!string.IsNullOrEmpty(roomName))
+            {
+                roomName = "Room" + Random.Range(100, 1000);
+            }
 
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = (byte)(maxPlayers);
+
+            //ns = nombre sala
+            //ps = password sala
+            string[] roomPropsInLobby = { "ns", "ps" };
+
+            ExitGames.Client.Photon.Hashtable customRoomProperties = new ExitGames.Client.Photon.Hashtable() { { "ns", roomName }, { "ps", pass } };
+            roomOptions.CustomRoomPropertiesForLobby = roomPropsInLobby;
+            roomOptions.CustomRoomProperties = customRoomProperties;
+
+            PhotonNetwork.CreateRoom(roomName, roomOptions);
+        }
+
+    }
     #endregion
 
     #region Photon Callbacks
@@ -92,6 +127,32 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         ActivatePanel(GameOptionsUIPanel.name);
         Debug.LogFormat("{0} Se ha conectado al servidor de Photon!", PhotonNetwork.LocalPlayer.NickName);
     }
+
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log(PhotonNetwork.CurrentRoom.Name + " has been created");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " has joined to: " + PhotonNetwork.CurrentRoom.Name);
+
+        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("ns") && PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("ps"))
+        {
+            object nombreSala;
+            object passSala;
+            if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("ns", out nombreSala))
+            {
+                Debug.Log(nombreSala.ToString());
+            }
+            if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("ps", out passSala))
+            {
+                Debug.Log(passSala.ToString());
+            }
+        }
+    }
+
     public override void OnDisconnected(DisconnectCause cause)
     {
         try
